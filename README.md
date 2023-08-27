05453eccd40a2731c8e86507b439124c6cf7b4e9
David Runion <david.runion@imedemr.com>
2023-08-27 00:19:58 -0400

    start of bucketing script

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 This repository contains a set of scripts to perform airdrop functions for the Voi blockchain network.
 
 There are three core scripts:
-# Initial airdrop script
-# Epoch reward calculaton script
-# Epoch airdrop script
+# Initial airdrop script - airdrop.js
+# Epoch reward calculaton script - epoch_calc.js
+# Account bucketing scirpt - buckets.js
 
 The three scripts are currently a work in progress. None of these are recommended for use.
---
+++ buckets.js
@@ -0,0 +1,50 @@
+/*
+
+	Script to put accounts into buckets based on their balance as a percentage of total tokens
+
+	Usage: node buckets.js
+
+	Status: INCOMPLETE
+
+*/
+
+import algosdk from 'algosdk';
+
+const c = new algosdk.Algodv2("", "https://testnet-api.algonode.cloud", "");
+const indexerClient = new algosdk.Indexer("", "https://testnet-idx.algonode.cloud", "");
+
+(async () => {
+    let balancesList = {};
+	let totalTokens = 0;
+    let nextToken = null;
+
+    do {
+        // Fetch accounts using the indexer (with a limit, e.g., 1000 accounts per request)
+        const response = await indexerClient.searchAccounts().limit(100).nextToken(nextToken).do();
+
+        for (const account of response.accounts) {
+            balancesList[account.address] = account.amount;
+			totalTokens += account.amount;
+        }
+
+        //nextToken = response['next-token'];
+    } while (nextToken);
+
+    console.log(balancesList);
+
+	let buckets = {};
+
+	// for each account in balances, calculate and place it in its bucket
+	for (const account in balancesList) {
+		const bal = balancesList[account];
+		const bucket = Math.floor((bal / totalTokens) * 100);
+		if (typeof buckets[bucket] == 'undefined') buckets[bucket] = [];
+		
+		buckets[bucket].push({
+			account: account,
+			amount: bal,
+		});
+	}
+
+	console.log(buckets);
+})();
