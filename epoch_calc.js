aa946bac2aadf3b606ffd64a2d683d242227a9d3
David Runion <david.runion@imedemr.com>
2023-08-28 20:12:11 -0400

    change epoch reward calc to use blocks, add util script find_block to search for block based on timestamp

--- epoch_calc.js
+++ epoch_calc.js
@@ -15,57 +15,23 @@ import algosdk from 'algosdk';
 import minimist from 'minimist';
 import { writeToCSV } from './utils.js';
 
-const default_epoch_start = '2023-08-25T00:00:00';
-const default_epoch_end = '2023-08-25T00:01:00';
+//const default_epoch_start = '2023-08-25T00:00:00';
+//const default_epoch_end = '2023-08-25T00:01:00';
 //const epoch_block_reward = 1000000;
-
-//const previous_block = 13819292; // TODO: read from DB, or on-chain tx
 
 const c = new algosdk.Algodv2("", "https://testnet-api.algonode.cloud", "");
 
 export const getFilenameArguments = () => {
     const args = minimist(process.argv.slice(2));
-    let epoch_start = (args.s)??=default_epoch_start; // TODO MAKE NULL
-    let epoch_end = (args.e)??=default_epoch_end; // TODO MAKE NULL
+    let start_block = (args.s)??=null;
+    let end_block = (args.e)??=null;
 	let epoch_block_reward = (args.r)??=0;
 	let output_filename = (args.f)??='epoch_rewards.csv';
-    return [ epoch_start, epoch_end, epoch_block_reward, output_filename ];
-}
-
-async function getClosestBlock(timestamp,lowerBound = 1) {
-    let upperBound = (await c.status().do())['last-round'];
-    //let lowerBound = 1; // Start from block 1, adjust if you have a better starting point
-
-    while (lowerBound <= upperBound) {
-        const midPoint = Math.floor((upperBound + lowerBound) / 2);
-        const block = await c.block(midPoint).do();
-        const blockTime = block.block.ts * 1000; // Convert from seconds to milliseconds
-
-        if (blockTime < timestamp) {
-            lowerBound = midPoint + 1;
-        } else if (blockTime > timestamp) {
-            upperBound = midPoint - 1;
-        } else {
-            return midPoint;  // Exact match, though this is unlikely
-        }
-    }
-
-    return lowerBound; // Returns block with timestamp just after the given timestamp
+    return [ start_block, end_block, epoch_block_reward, output_filename ];
 }
 
 (async () => {
-	const [ epoch_start, epoch_end, epoch_block_reward, output_filename ] = getFilenameArguments();
-
-    const startDate = new Date(epoch_start).getTime();
-    const endDate = new Date(epoch_end).getTime() - 1000; // one second less to avoid conflict with next epoch
-
-	process.stdout.write(`Calculating Epoch starting block... `);
-    const start_block = await getClosestBlock(startDate);
-	console.log(start_block);
-
-	process.stdout.write(`Calculating Epoch ending block... `);
-	const end_block = await getClosestBlock(endDate,start_block)-1; // subtract one to get 
-	console.log(end_block);
+	const [ start_block, end_block, epoch_block_reward, output_filename ] = getFilenameArguments();
 
 	const epoch_total_blocks = end_block-start_block+1;
 	console.log(`Total blocks produced in Epoch: ${epoch_total_blocks}`);
---
+++ find_block.js
@@ -0,0 +1,63 @@
+/*
+
+	Script to find the next block following a specified date/time using a binary search.
+	Time must be formatted as YYYY-MM-DDTHH:MM:SS, i.e. 2023-08-28T13:04:33
+
+	Usage: node find_block.js -t TIMESTAMP
+
+*/
+
+import algosdk from 'algosdk';
+import minimist from 'minimist';
+
+const c = new algosdk.Algodv2("", "https://testnet-api.algonode.cloud", "");
+
+export const getFilenameArguments = () => {
+    const args = minimist(process.argv.slice(2));
+    let timestamp = (args.t)??=0;
+    return [ timestamp ];
+}
+
+async function getClosestBlock(timestamp,lowerBound = 1) {
+    let upperBound = (await c.status().do())['last-round'];
+    //let lowerBound = 1; // Start from block 1, adjust if you have a better starting point
+
+    while (lowerBound <= upperBound) {
+        const midPoint = Math.floor((upperBound + lowerBound) / 2);
+        const block = await c.block(midPoint).do();
+        const blockTime = block.block.ts * 1000; // Convert from seconds to milliseconds
+
+        if (blockTime < timestamp) {
+            lowerBound = midPoint + 1;
+        } else if (blockTime > timestamp) {
+            upperBound = midPoint - 1;
+        } else {
+            return midPoint;  // Exact match, though this is unlikely
+        }
+    }
+
+    return lowerBound; // Returns block with timestamp just after the given timestamp
+}
+
+(async () => {
+	const [ timestamp ] = getFilenameArguments();
+
+	if (timestamp == 0) {
+		console.log('GMT Timestamp of format YYYY-MM-DDTHH:MM:SS required using -t');
+		console.log('Usage: node find_block.js -t 2023-08-28T13:04:33');
+		process.exit();
+	}
+
+    const useTime = new Date(timestamp).getTime();
+
+	process.stdout.write(`Calculating block at ${timestamp} ... `);
+    const block = await getClosestBlock(useTime);
+	console.log(block);
+
+	// get actual block time
+	const detail = await c.block(block).do();
+	const tm = new Date(detail.block.ts * 1000).toLocaleString();
+	console.log(`Actual block time: ${tm}`);
+
+	process.exit();
+})();
