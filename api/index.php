5a9b5a6559b5f18a0bd3486588756c4a9091fae1
David Runion <david.runion@imedemr.com>
2023-10-01 15:03:56 -0400

    add health data to api

--- api/index.php
+++ api/index.php
@@ -24,6 +24,76 @@ function fetchBlacklist() {
     return $combinedAddresses;
 }
 
+function fetchWeeklyHealth($blacklist) {
+    $weeklyHealthEndpoint = 'health_week.json';
+
+    $response = file_get_contents($weeklyHealthEndpoint);
+    if (!$response) {
+        throw new Exception('HTTP error!');
+    }
+
+    $jsonData = json_decode($response, true);
+
+    $meta = $jsonData['meta'];
+    $data = $jsonData['data'];
+
+    $positions = array('host'=>null,'name'=>null,'score'=>null,'addresses'=>array());
+    foreach($meta as $pos=>$m) {
+        $positions[$m['name']] = $pos;
+    }
+
+    $nodes = array();
+    $totalNodeCount = 0;
+    $healthyNodeCount = 0;
+    $emptyNodeCount = 0;
+
+    foreach($data as $d) {
+        foreach($d[$positions['addresses']] as $pos=>$address) {
+            if (in_array($address, $blacklist)) {
+                unset($d[$positions['addresses']][$pos]);
+            }
+        }
+
+        $nodes[] = array(
+            'host' => $d[$positions['host']],
+            'name' => $d[$positions['name']],
+            'score' => $d[$positions['score']],
+            'addresses' => $d[$positions['addresses']],
+        );
+
+        if ($d[$positions['score']] >= 5.0) {
+            $healthyNodeCount++;
+        }
+
+        $totalNodeCount++;
+    }
+
+    // map $nodes array to use addresses as keys
+    $addresses = array();
+    foreach($nodes as $node) {
+        if (count($node['addresses']) == 0 && $node['score'] >= 5.0) {
+            $emptyNodeCount++;
+        }
+        foreach($node['addresses'] as $address) {
+            if (isset($addresses[$address])) {
+                $addresses[$address]['divisor'] = min(count($node['addresses'],$addresses[$address]['divisor']));
+            }
+            else {
+                $addresses[$address] = $node;
+                $addresses[$address]['divisor'] = count($node['addresses']);
+                unset($addresses[$address]['addresses']);
+            }
+        }
+    }
+
+    return array(
+        'addresses'=>$addresses,
+        'total_node_count'=>$totalNodeCount,
+        'healthy_node_count'=>$healthyNodeCount,
+        'empty_node_count'=>$emptyNodeCount,
+    );
+}
+
 // Get the start and end timestamps from the GET request
 $startTimestamp = (isset($_GET['start'])) ? $_GET['start'].'T00:00:00Z' : null;
 $endTimestamp = (isset($_GET['end'])) ? $_GET['end'].'T23:59:59Z' : null;
@@ -62,6 +132,9 @@ $data = array();
 // Fetch the blacklist
 $blacklist = fetchBlacklist();
 
+// Fetch weekly health data
+$health = fetchWeeklyHealth($blacklist);
+
 // Loop through the results and add the data to the array
 while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     if (in_array($row['proposer'], $blacklist)) {
@@ -69,7 +142,32 @@ while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
     }
     $data[] = array(
         'proposer' => $row['proposer'],
-        'block_count' => $row['block_count']
+        'block_count' => $row['block_count'],
+        'node' => array(
+            'node_host' => isset($health['addresses'][$row['proposer']]) ? $health['addresses'][$row['proposer']]['host'] : null,
+            'node_name' => isset($health['addresses'][$row['proposer']]) ? $health['addresses'][$row['proposer']]['name'] : null,
+            'health_score' => isset($health['addresses'][$row['proposer']]) ? $health['addresses'][$row['proposer']]['score'] : null,
+            'health_divisor' => isset($health['addresses'][$row['proposer']]) ? $health['addresses'][$row['proposer']]['divisor'] : null,
+        ),
+    );
+
+    // remove so we can merge in remaining nodes
+    if (isset($health['addresses'][$row['proposer']])) {
+        unset($health['addresses'][$row['proposer']]);
+    }
+}
+
+// Add remaining nodes to the data array
+foreach($health['addresses'] as $address=>$node) {
+    $data[] = array(
+        'proposer' => $address,
+        'block_count' => 0,
+        'node' => array(
+            'node_host' => $node['host'],
+            'node_name' => $node['name'],
+            'health_score' => $node['score'],
+            'health_divisor' => $node['divisor'],
+        ),
     );
 }
 
@@ -88,6 +186,9 @@ $output = array(
     'data' => $data,
     'max_timestamp' => $maxTimestamp,
     'block_height' => $blockHeightResult,
+    'total_node_count' => $health['total_node_count'],
+    'healthy_node_count' => $health['healthy_node_count'],
+    'empty_node_count' => $health['empty_node_count'],
 );
 
 // Convert the output to a JSON object and output it
